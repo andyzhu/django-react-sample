daa149f922dc92fe7e4bfdf7ba4ab8eaf3d0d57b
andyzhu612@gmail.com <Nanjing@3910>
2021-02-07 10:33:20 -0500

    added simple object creation test

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Book
 
 # Create your tests here.
+class BookTestCase(TestCase):
+    def test_book_created(self):
+        book_obj = Book.objects.create(name='Anon', author='John Doe', description='anon book')
+        self.assertEqual(book_obj.id, 1)
